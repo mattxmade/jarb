48e1a29bff942969031d218cd77fb8336363026d
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-14 11:32:42 +0100

    Configure build output

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,22 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `js/[name].js`,
+                chunkFileNames: `js/[name].js`,
+                assetFileNames: `css/[name].[ext]`,
+            },
+        },
+    },
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
+            input: ["resources/css/app.css", "resources/js/index.jsx"],
             refresh: true,
         }),
         react(),
     ],
 });
+
+// no hash names | https://stackoverflow.com/a/75344943
