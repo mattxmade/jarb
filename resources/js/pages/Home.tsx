7fc9137022d9829d8b6eeb02c2baf93719af9c10
Matt <97047080+mattxmade@users.noreply.github.com>
2023-10-26 18:53:49 +0100

    Complete pending status implementation; pass status props to child Components

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Head, usePage } from "@inertiajs/react";
+import { Head, router, usePage } from "@inertiajs/react";
 
 import { JobSearchFields, JobSearchResult, ReedSearchResponse } from "../types";
 
@@ -7,10 +7,16 @@ import { meta } from "../../content/content.json";
 
 import JobSearchForm from "../components/JobSearchForm";
 import useLoadscreen from "../hooks/useLoadscreen";
+import { useCallback, useRef, useState } from "react";
 
 type AppProps = {
     jobDetailsResponse?: any;
     searchResponse?: ReedSearchResponse;
+};
+
+const restoreItem = (key: string, fallback: any) => {
+    const item = router.restore(key);
+    return typeof item === "string" ? JSON.parse(item) : fallback;
 };
 
 export default function Home(props: AppProps) {
@@ -31,13 +37,31 @@ export default function Home(props: AppProps) {
 
     const results = props.searchResponse || resultsCache || undefined;
 
+    const [status, setStatus] = useState(() => restoreItem("status", "ready"));
+
+    const handleStatus = useCallback(
+        (status: string) => {
+            setStatus(status);
+            router.remember("status", status);
+        },
+        [status]
+    );
+
+    const onLoad = setTimeout(() => {
+        clearTimeout(onLoad);
+        if (results && status === "pending") setStatus("ready");
+    }, 1000);
+
     return (
-        <App jobSearchResponse={results}>
+        <App jobSearchResponse={results} status={status}>
             <Head>
                 <title>{meta.title}</title>
                 <meta name={meta.name} content={meta.content} />
             </Head>
-            <JobSearchForm searchResponse={results} />
+            <JobSearchForm
+                searchResponse={results}
+                handleStatus={handleStatus}
+            />
         </App>
     );
 }
