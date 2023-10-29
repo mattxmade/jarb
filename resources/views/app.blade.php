3558768128270f94aef8b6257a26abe8983ce360
Matt <97047080+mattxmade@users.noreply.github.com>
2023-10-29 21:13:37 +0000

    Update app.blade.php: Update favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" type="text/css">
-        <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.svg') }}">
         @viteReactRefresh
         @vite(['resources/sass/app.scss', 'resources/js/index.jsx'])
         @inertiaHead
