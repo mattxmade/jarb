03af96b15922273863f1f241c4c189013697049d
Matt <97047080+mattxmade@users.noreply.github.com>
2023-11-08 16:43:41 +0000

    Create View Component: create params; create instance + decode json; return view renderer

---
+++ app/View/Components/Loadscreen.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Loadscreen extends Component
+{
+    /**
+     * Loadscreen title.
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * Loadscreen HTML elements.
+     *
+     * @var araay
+     */
+    public $html = array();
+
+    /**
+     * Loadscreen content.
+     *
+     * @var object
+     */
+    public $content;
+ 
+    /**
+     * Create the component instance.
+     *
+     * @param  string $title
+     * @param  array  $html
+     * @param  object $content
+     * @return void
+     */
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+        $this->content = json_decode(file_get_contents(database_path('data/content.json')));
+        $this->title = $this->content->{'title'};
+        $this->html = $this->content->{'html'};
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+      return view('components.loadscreen');
+    }
+}
