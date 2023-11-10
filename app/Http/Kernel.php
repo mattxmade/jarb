006c4246666acf18e5e8bc68c9f5c8e86132287d
Matt <97047080+mattxmade@users.noreply.github.com>
2023-11-10 17:35:50 +0000

    Update Kernel.php: Add web CSP middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
+            \App\Http\Middleware\AddContentSecurityPolicyHeaders::class
         ],
 
         'api' => [
@@ -65,5 +66,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
+      ];
 }
